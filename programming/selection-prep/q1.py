0b800f23fc959a4d615b29102b8d40cf320d7c36
Steven Carroll <32237935+dc-carrolls@users.noreply.github.com>
2024-09-16 09:50:51 +0100

    Feedback: Selection Prep
    Please use the coding conventions and you also need to include the classwork/worksheet code in github

--- programming/selection-prep/q1.py
+++ programming/selection-prep/q1.py
@@ -1,5 +1,7 @@
+### SRC - Please use the coding conventions, i.e. #end if
 name = str(input("enter your name: "))
 if name == "Hazel":
     print("Hello Hazel")
 else:
     print("We haven't met yet " + name + ", nice to meet you")
+#end if
