0e6380d44bd601c3b52c67b53575f6e512c531b6
Steven Carroll <32237935+dc-carrolls@users.noreply.github.com>
2024-10-21 08:08:05 +0100

    Feedback: Snow3
    Look at the comments in the code. Remember, Flake is not a variable, but the definition of a Flake record. You have to create the Flakes and add them to a 1D array. You can then access each flake through the array.

--- programming/pygame/snow3.0.py
+++ programming/pygame/snow3.0.py
@@ -10,7 +10,7 @@ x_pos = random.randint(0,700)
 y_pos = random.randint(0, 500)
 velocity = random.randint(1,10)
 
-class flake: # this is a record
+class Flake: # this is a record
         def __init__(self,x_pos,y_pos,velocity,size) -> None:
              self.x = x_pos
              self.y = y_pos
@@ -27,13 +27,20 @@ screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Snow")
 
+### SRC - This code sets up a 2D array, one dimension for the rows
+### The other dimension stores the 3 values for the snowflake.
+### You need to change this to be just a 1D array. 
 rows = 50
-cols = 3   
-arr = [[0 for i in range(cols)] for j in range(rows)]  
+cols = 3   # Remove this line
+arr = [[0 for i in range(cols)] for j in range(rows)]  # Change this line to have 50 values of None 
 
 
+### SRC - This is where we are updating the array to have random values for our flakes
 
 for row in range(rows):  
+     # SRC - I've added the code to create a Flake record below:
+     my_flake = Flake(x,y,v,s) # SRC - You need to set the right values for x, y, v and s
+     ### then change the lines below to add my_flake to the 1D array. 
      arr[row][0] = random.randint(0,size[0]-1)
      arr[row][1] = random.randint(0,size[1]-1)
      arr[row][2] = 1
@@ -54,8 +61,9 @@ while not done:
 
     # --- Game logic should go here
 
+
     for i in range(len(arr)):
-        if flake.y > size[1]:
+        if flake.y > size[1]: ### It#s not flake.y but arr[i].y etc.
               flake.y = 0
               flake.x = random.randint(0,size[0]-1)
         else:
@@ -71,6 +79,7 @@ while not done:
 
 
     # --- Drawing code should go here
+    ### Again here it's not flake.x etc you need to access the flakes in the array.
     for i in range(len(arr)):
         pygame.draw.rect(screen, WHITE, (flake.x,flake.y,flake.size,flake.size))
 
